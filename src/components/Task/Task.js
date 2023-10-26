a2eead30ebc158eebed3528939728d13319d9440
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-26 17:09:13 +0200

    3)Hooks : add Drag n Drop function with HTML5 API

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,21 @@
 import './Task.css'
 
-function Task({ title, body}) {
+function Task({ id, title, body, onDragStart }) {
+
+    console.log(onDragStart)
+
+    function onDragEnter(e) {
+        e.preventDefault();
+    }
+
   return (
-    <div className='Task-wrapper'>
-       <h3>Task</h3>
+    <div 
+        className='Task-wrapper'
+        draggable='true'
+        onDragStart={(e) => onDragStart(e, id)}
+        onDragEnter={onDragEnter}
+    >
+       <h3>{title}</h3>
        <p>{body}</p> 
     </div>
   )
